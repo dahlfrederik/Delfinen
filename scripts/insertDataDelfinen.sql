c26be58d43ee5a97762fcb03ffa1d7b25af16d43
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2019-12-04 16:19:29 +0100

    insertData

---
+++ scripts/insertDataDelfinen.sql
@@ -0,0 +1,92 @@
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Simon", 21948822, 22, "Senior", true, true);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Tom", 29119933, 17, "Junior", true, true);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Lucas", 34991133, 27, "Senior", false, false);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Nikolej", 55118833, 85, "Senior", true, true);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Bjarne", 99338811, 16, "Junior", true, false);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Mario", 99881133, 15,"Junior", true, true);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Jonas", 81993388, 16, "Junior", true, false);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Steve", 9911833, 14, "Junior", true, true);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Elon", 18338291, 55, "Senior", true, true);
+INSERT INTO members (name, phonenr, age, team, active, paystatus) VALUES ("Bill", 94821199, 65, "Senior", true, true);
+
+INSERT INTO results (name, date, comp, disc, result) VALUES ("Simon",  03122019, true, "Butterfly", 2.23);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Tom", 04122019, true, "Butterfly", 2.55);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Nikolej", 02122019, true, "Butterfly", 3.42);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bjarne", 02122019, true, "Butterfly", 2.24);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Mario", 03122019, true, "Butterfly", 1.95);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Jonas", 0312019, true, "Butterfly", 2.14);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Steve", 02122019, true, "Butterfly", 2.53);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Elon", 01122019, true, "Butterfly", 2.33);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bill", 02122019, true, "Butterfly", 2.22);
+
+INSERT INTO results (name,date, comp, disc, result) VALUES ("Simon",  03122019, true, "Crawl", 1.45);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Tom", 04122019, true, "Crawl", 1.25);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Nikolej", 02122019, true, "Crawl", 1.42);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bjarne", 02122019, true, "Crawl", 1.24);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Mario", 03122019, true, "Crawl", 1.21);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Jonas", 0312019, true, "Crawl", 2.25);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Steve", 02122019, true, "Crawl", 1.53);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Elon", 01122019, true, "Crawl", 1.33);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bill", 02122019, true, "Crawl", 1.22);
+
+INSERT INTO results (name,date, comp, disc, result) VALUES ("Simon",  03122019, true, "Rygcrawl", 0.45);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Tom", 04122019, true, "Rygcrawl", 0.53);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Nikolej", 02122019, true, "Rygcrawl", 1.22);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bjarne", 02122019, true, "Rygcrawl", 1.52);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Mario", 03122019, true, "Rygcrawl", 1.00);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Jonas", 0312019, true, "Rygcrawl", 0.55);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Steve", 02122019, true, "Rygcrawl", 0.53);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Elon", 01122019, true, "Rygcrawl", 0.33);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bill", 02122019, true, "Rygcrawl", 0.42);
+
+INSERT INTO results (name,date, comp, disc, result) VALUES ("Simon",  03122019, true, "Brystsvømning", 1.22);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Tom", 04122019, true, "Brystsvømning", 1.52);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Nikolej", 02122019, true, "Brystsvømning", 1.32);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bjarne", 02122019, true, "Brystsvømning", 1.22);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Mario", 03122019, true, "Brystsvømning", 1.21);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Jonas", 0312019, true, "Brystsvømning", 1.12);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Steve", 02122019, true, "Brystsvømning", 1.27);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Elon", 01122019, true, "Brystsvømning", 1.55);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bill", 02122019, true, "Brystsvømning", 1.51);
+
+-- Ikke comp
+
+INSERT INTO results (name, date, comp, disc, result) VALUES ("Simon",  03122019, false, "Butterfly", 3.22);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Tom", 04122019, false, "Butterfly", 4.55);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Nikolej", 02122019, false, "Butterfly", 2.12);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bjarne", 02122019, false, "Butterfly", 1.23);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Mario", 03122019, false, "Butterfly", 3.95);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Jonas", 0312019, false, "Butterfly", 3.14);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Steve", 02122019, false, "Butterfly", 3.23);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Elon", 01122019, false, "Butterfly", 3.31);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bill", 02122019, false, "Butterfly", 3.22);
+
+INSERT INTO results (name,date, comp, disc, result) VALUES ("Simon",  03122019, false, "Crawl", 2.23);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Tom", 04122019, false, "Crawl", 2.15);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Nikolej", 02122019, false, "Crawl", 2.42);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bjarne", 02122019, false, "Crawl", 3.22);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Mario", 03122019, false, "Crawl", 1.21);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Jonas", 0312019, false, "Crawl", 2.25);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Steve", 02122019, false, "Crawl", 4.53);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Elon", 01122019, false, "Crawl", 5.33);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bill", 02122019, false, "Crawl", 3.22);
+
+INSERT INTO results (name,date, comp, disc, result) VALUES ("Simon",  03122019, false, "Rygcrawl", 1.25);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Tom", 04122019, false, "Rygcrawl", 1.23);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Nikolej", 02122019, false, "Rygcrawl", 2.22);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bjarne", 02122019, false, "Rygcrawl", 3.11);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Mario", 03122019, false, "Rygcrawl", 2.12);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Jonas", 0312019, false, "Rygcrawl", 1.52);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Steve", 02122019, false, "Rygcrawl", 1.53);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Elon", 01122019, false, "Rygcrawl", 0.55);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bill", 02122019, false, "Rygcrawl", 1.32);
+
+INSERT INTO results (name,date, comp, disc, result) VALUES ("Simon",  03122019, false, "Brystsvømning", 2.23);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Tom", 04122019, false, "Brystsvømning", 1.32);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Nikolej", 02122019, false, "Brystsvømning", 2.11);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bjarne", 02122019, false, "Brystsvømning", 1.42);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Mario", 03122019, false, "Brystsvømning", 2.23);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Jonas", 0312019, false, "Brystsvømning", 0.45);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Steve", 02122019, false, "Brystsvømning", 1.55);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Elon", 01122019, false, "Brystsvømning", 2.23);
+INSERT INTO results (name,date,comp,disc,result) VALUES ("Bill", 02122019, false, "Brystsvømning", 2.33);
